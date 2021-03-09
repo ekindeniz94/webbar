b866e20775d722b77db81830af12e22016e8024a
Benedikt Iltisberger <iltisberger@gmail.com>
2021-03-09 11:13:29 +0100

    fix: added missing properties to billing-entry.

--- src/plugins/mo-billing/interfaces/bill-entry.data.ts
+++ src/plugins/mo-billing/interfaces/bill-entry.data.ts
@@ -1,6 +1,8 @@
 import { TaxEnum } from "../enums/tax.enum";
 
 export interface IBillEntryData {
+    name: string;
+    description: string;
     quantity: number;
     pricePerQuantityInCent: number;
     tax: TaxEnum; 
--- src/plugins/mo-billing/models/bill-entry.model.ts
+++ src/plugins/mo-billing/models/bill-entry.model.ts
@@ -2,11 +2,15 @@ import { TaxEnum } from '../enums/tax.enum';
 import { IBillEntryData } from '../interfaces';
 
 export class BillEntryModel {
+  protected _name: string;
+  protected _description: string;
   protected _quantity: number;
   protected _pricePerQuantityInCent: number;
   protected _tax: TaxEnum;
 
   constructor(data: IBillEntryData) {
+    this._name = data.name;
+    this._description = data.description;
     this._quantity = data.quantity;
     this._pricePerQuantityInCent = data.pricePerQuantityInCent;
     this._tax = data.tax;
@@ -14,6 +18,8 @@ export class BillEntryModel {
 
   public getSerialized() {
     return {
+      name: this._name,
+      description: this._description,
       quantity: this._quantity,
       pricePerQuantityInCent: this._pricePerQuantityInCent,
       tax: this._tax
