d9cf6da53f7a51b2b9dc00b8cfe9f4e3ed629173
Benedikt Iltisberger <iltisberger@gmail.com>
2021-05-19 17:19:17 +0200

    fix: added first and lastname to register.

--- src/modules/mo-user/data-transfer-objects/user/user-register-request.dto.ts
+++ src/modules/mo-user/data-transfer-objects/user/user-register-request.dto.ts
@@ -1,8 +1,26 @@
-import { IsEmail, IsNotEmpty, isString, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import { Expose, Transform } from 'class-transformer';
-import { DTO_VALIDATION_CONST, IsInStringList } from '../../../mo-core';
+import { IsEmail, IsNotEmpty, isString, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { DTO_VALIDATION_CONST } from '../../../mo-core';
 
 export class UserRegisterRequestDto {
+  @IsNotEmpty()
+  @MinLength(DTO_VALIDATION_CONST.FIRST_NAME.MIN)
+  @MaxLength(DTO_VALIDATION_CONST.FIRST_NAME.MAX)
+  @Transform(({ value }) =>
+    (value && isString(value) ? value.trim() : value)?.substring(0, DTO_VALIDATION_CONST.FIRST_NAME.MAX)
+  )
+  @Expose()
+  firstName: string;
+
+  @IsNotEmpty()
+  @MinLength(DTO_VALIDATION_CONST.LAST_NAME.MIN)
+  @MaxLength(DTO_VALIDATION_CONST.LAST_NAME.MAX)
+  @Transform(({ value }) =>
+    (value && isString(value) ? value.trim() : value)?.substring(0, DTO_VALIDATION_CONST.LAST_NAME.MAX)
+  )
+  @Expose()
+  lastName: string;
+
   @IsNotEmpty()
   @IsEmail()
   @MinLength(DTO_VALIDATION_CONST.EMAIL.MIN)
