3713df7b6eb4acacd4d807dadcf3d6330c7aaa65
Behrang Alavi <behrang@mogenius.com>
2021-07-08 11:28:32 +0200

    fix: add hasPermission and hasPermissions to GroupDto

--- src/modules/mo-user/data-transfer-objects/group/group.dto.ts
+++ src/modules/mo-user/data-transfer-objects/group/group.dto.ts
@@ -13,4 +13,12 @@ export class GroupDto extends BaseEntityDto {
 
   @Expose()
   permissions: string[];
+
+  public hasPermission(permission: string): boolean {
+    return !!this.permissions.find((item: string) => item === permission);
+  }
+  
+  public hasPermissions(permissions: string[]): boolean {
+    return !!permissions.find((item: string) => this.hasPermission(item));
+  }
 }
