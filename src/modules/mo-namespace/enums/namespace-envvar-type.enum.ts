7db7404d875b32a742cff0d981ee123a46e6dd90
Mogenius git-user <git@mogenius.com>
2021-07-12 15:26:52 +0200

    fix: new entry in env-var-enum volumemount.

--- src/modules/mo-namespace/enums/namespace-envvar-type.enum.ts
+++ src/modules/mo-namespace/enums/namespace-envvar-type.enum.ts
@@ -1,9 +1,11 @@
 export enum NamespaceServiceEnvVarTypeEnum {
   PLAINTEXT = 'PLAINTEXT',
-  KEYVAULT = 'KEYVAULT'
+  KEYVAULT = 'KEYVAULT',
+  VOLUME_MOUNT = 'VOLUME_MOUNT'
 }
 
 export const ALL_NAMESPACE_SERVICE_ENVVAR_TYPES = [
   NamespaceServiceEnvVarTypeEnum.PLAINTEXT,
-  NamespaceServiceEnvVarTypeEnum.KEYVAULT
+  NamespaceServiceEnvVarTypeEnum.KEYVAULT,
+  NamespaceServiceEnvVarTypeEnum.VOLUME_MOUNT
 ];
