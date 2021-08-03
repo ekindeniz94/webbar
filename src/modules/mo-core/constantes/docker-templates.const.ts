1d21c4514871396504d50e668a96994b7c6ebbc3
mogenius git-user <git@mogenius.com>
2021-08-03 11:12:13 +0200

    fix: added mongodb-express and mongo.

--- src/modules/mo-core/constantes/docker-templates.const.ts
+++ src/modules/mo-core/constantes/docker-templates.const.ts
@@ -163,7 +163,7 @@ export const DOCKER_TEMPLATES: IDockerTemplate[] = [
     id: 'mongo-4.4.3-bionic',
     version: '4.4.3-bionic',
     description:
-      'MongoDB is a free and open-source cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with schemata. MongoDB is developed by MongoDB Inc., and is published under a combination of the Server Side Public License and the Apache License. First developed by the software company 10gen (now MongoDB Inc.) in October 2007 as a component of a planned platform as a service product, the company shifted to an open source development model in 2009, with 10gen offering commercial support and other services. Since then, MongoDB has been adopted as backend software by a number of major websites and services, including MetLife, Barclays, ADP, UPS, Viacom, and the New York Times, among others. MongoDB is the most popular NoSQL database system.',
+      'MongoDB is a free and open-source cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with schemata. MongoDB is developed by MongoDB Inc., and is published under a combination of the Server Side Public License and the Apache License.',
     folder: 'mongo',
     lastUpdate: '2021-02-01T09:50:31+01:00',
     envVars: [
