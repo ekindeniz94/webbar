1d16e72621606c55c32e6014728e923a0eafdd03
mogenius git-user <git@mogenius.com>
2021-08-03 10:50:55 +0200

    fix: added mongodb.

--- src/modules/mo-core/constantes/docker-templates.const.ts
+++ src/modules/mo-core/constantes/docker-templates.const.ts
@@ -166,6 +166,24 @@ export const DOCKER_TEMPLATES: IDockerTemplate[] = [
       'MongoDB is a free and open-source cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with schemata. MongoDB is developed by MongoDB Inc., and is published under a combination of the Server Side Public License and the Apache License. First developed by the software company 10gen (now MongoDB Inc.) in October 2007 as a component of a planned platform as a service product, the company shifted to an open source development model in 2009, with 10gen offering commercial support and other services. Since then, MongoDB has been adopted as backend software by a number of major websites and services, including MetLife, Barclays, ADP, UPS, Viacom, and the New York Times, among others. MongoDB is the most popular NoSQL database system.',
     folder: 'mongo',
     lastUpdate: '2021-02-01T09:50:31+01:00',
+    envVars: [
+      {
+        name: 'MONGO_INITDB_ROOT_USERNAME',
+        value: 'root',
+        type: NamespaceServiceEnvVarTypeEnum.PLAINTEXT,
+        deactivateName: true,
+        deactivateValue: false,
+        deactivateType: true,
+      },
+      {
+        name: 'MONGO_INITDB_ROOT_PASSWORD',
+        value: '',
+        type: NamespaceServiceEnvVarTypeEnum.KEY_VAULT,
+        deactivateName: true,
+        deactivateValue: false,
+        deactivateType: true,
+      }
+    ],
     internalPort: 27017,
     expose: false
   },
