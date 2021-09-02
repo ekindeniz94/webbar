7bf7a4836a041ba9238612b495c75b95cd815c91
mogenius git-user <git@mogenius.com>
2021-09-02 15:00:26 +0200

    fix: product and plan modified.

--- src/modules/mo-plan/enums/index.ts
+++ src/modules/mo-plan/enums/index.ts
@@ -1,3 +1,5 @@
 export * from './product-runtime-interval.enum';
 export * from './product-type.enum';
-
+export * from './paypal-category-type.enum';
+export * from './paypal-product-type.enum';
+export * from './plan-state.enum';
