f05078d9e671ac1d47f0372bd698f935bbd26472
mogenius git-user <git@mogenius.com>
2021-10-05 20:30:51 +0200

    fix: added free product type.

--- src/modules/mo-plan/enums/product-type.enum.ts
+++ src/modules/mo-plan/enums/product-type.enum.ts
@@ -1,11 +1,13 @@
 export enum ProductTypeEnum {
   STANDALONE = 'STANDALONE',
   PLAN = 'PLAN',
-  BUNDLE = 'BUNDLE'
+  BUNDLE = 'BUNDLE',
+  FREE = 'FREE'
 }
 
 export const ProductTypeEnum_ALL = [
   ProductTypeEnum.STANDALONE,
   ProductTypeEnum.PLAN,
-  ProductTypeEnum.BUNDLE
+  ProductTypeEnum.BUNDLE,
+  ProductTypeEnum.FREE
 ];
