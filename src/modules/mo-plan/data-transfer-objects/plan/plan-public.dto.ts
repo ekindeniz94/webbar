ce6041da645d42d30d5d70703069f0c5d64eb4c0
Behrang Alavi <behrang@mogenius.com>
2021-10-07 15:44:38 +0200

    fix: update plan public dto

--- src/modules/mo-plan/data-transfer-objects/plan/plan-public.dto.ts
+++ src/modules/mo-plan/data-transfer-objects/plan/plan-public.dto.ts
@@ -1,11 +1,20 @@
-import { Exclude, Expose } from 'class-transformer';
-import { CurrencyDto } from '../currency';
+import { Exclude, Expose, Transform, Type } from 'class-transformer';
 import { PlanDto } from './plan.dto';
+import { SubscriptionDto } from '../../../mo-subscription-pool';
+import { CurrencyDto } from '../currency';
+import { ProductRuntimeIntervalEnum } from '../../enums';
 
 export class PlanPublicDto extends PlanDto {
   @Exclude()
-  currencies: CurrencyDto[];
+  subscriptions: SubscriptionDto[];
 
+  @Type(() => CurrencyDto)
+  @Transform(({ value, obj }) => {
+    const mouth = obj.currencies?.find((item: CurrencyDto) => item.interval === ProductRuntimeIntervalEnum.MONTH);
+
+    const year = obj.currencies?.find((item: CurrencyDto) => item.interval === ProductRuntimeIntervalEnum.YEAR);
+    return year || mouth;
+  })
   @Expose()
   currency: CurrencyDto;
 }
