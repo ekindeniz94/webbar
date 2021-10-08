ce78023c41e38982a1b421ec7256ed6f79d11353
mogenius git-user <git@mogenius.com>
2021-10-08 14:26:20 +0200

    fix: cluster stuff.

--- src/modules/mo-plan/data-transfer-objects/product/index.ts
+++ src/modules/mo-plan/data-transfer-objects/product/index.ts
@@ -1,3 +1,5 @@
-export * from './product.dto';
+export * from './cluster.dto';
 export * from './product-create-request.dto';
 export * from './product-patch-request.dto';
+export * from './product.dto';
+
