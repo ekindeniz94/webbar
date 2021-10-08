e617a75b107ab55174e97ace3d5235f1c4d97a3f
mogenius git-user <git@mogenius.com>
2021-10-08 09:26:20 +0200

    fix: added color enum.

--- src/modules/mo-namespace/enums/index.ts
+++ src/modules/mo-namespace/enums/index.ts
@@ -1,9 +1,11 @@
 export * from './cluster-region.enum';
 export * from './namespace-bg-color-style.enum';
+export * from './namespace-color.enum';
 export * from './namespace-command-state.enum';
+export * from './namespace-envvar-type.enum';
 export * from './namespace-service-deployment-strategy.enum';
 export * from './namespace-service-type.enum';
 export * from './namespace-state.enum';
-export * from './namespace-envvar-type.enum';
 export * from './pod-stats-unit.enum';
 export * from './socket-message';
+
