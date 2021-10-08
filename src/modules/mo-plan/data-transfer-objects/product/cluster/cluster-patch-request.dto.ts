3407f502eb6c30c4a099526a9a137c694f212f17
Behrang Alavi <behrang@mogenius.com>
2021-10-08 16:22:30 +0200

    fix: add id to cluster patch request dto

--- src/modules/mo-plan/data-transfer-objects/product/cluster/cluster-patch-request.dto.ts
+++ src/modules/mo-plan/data-transfer-objects/product/cluster/cluster-patch-request.dto.ts
@@ -1,3 +1,11 @@
 import { ClusterCreateRequestDto } from './cluster-create-request.dto';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Expose } from 'class-transformer';
 
-export class ClusterPatchRequestDto extends ClusterCreateRequestDto {}
+export class ClusterPatchRequestDto extends ClusterCreateRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  @Expose()
+  id: string;
+}
