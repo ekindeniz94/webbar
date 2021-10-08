c42b732e8299ea9d1de4aaca3e47e4828efcbdff
mogenius git-user <git@mogenius.com>
2021-10-08 13:48:09 +0200

    fix: minor fix.

--- src/modules/mo-namespace/data-transfer-objects/kubernetes/cluster.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/kubernetes/cluster.dto.ts
@@ -1,14 +1,19 @@
-import { ClusterRegionEnum } from '../../enums';
 import { Expose } from 'class-transformer';
+import { ClusterRegionEnum } from '../../enums';
 
 export class ClusterDto {
   @Expose()
   region: ClusterRegionEnum;
+
   @Expose()
   name: string;
+
+  @Expose()
+  loadbalancerIp: string;
 }
 
 export const DEFAULT_KUBERNETES_CLUSTER: ClusterDto = {
-  region: ClusterRegionEnum.CENTRAL_EUROPE,
-  name: 'mogenius-prod-cluster'
+  region: ClusterRegionEnum.CENTRAL_GERMANY,
+  name: 'mogenius-free',
+  loadbalancerIp: '20.79.116.149'
 };
