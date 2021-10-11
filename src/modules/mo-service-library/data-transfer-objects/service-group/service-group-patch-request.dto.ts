340934b9db1a92188921bf8a7b062884ea98461e
mogenius git-user <git@mogenius.com>
2021-10-11 20:03:29 +0200

    fix: updated service group patch request.

--- src/modules/mo-service-library/data-transfer-objects/service-group/service-group-patch-request.dto.ts
+++ src/modules/mo-service-library/data-transfer-objects/service-group/service-group-patch-request.dto.ts
@@ -1,4 +1,11 @@
-import { ServiceGroupCreateRequestDto } from "./service-group-create-request.dto";
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { ServiceGroupCreateRequestDto } from './service-group-create-request.dto';
 
 export class ServiceGroupPatchRequestDto extends ServiceGroupCreateRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  @Expose()
+  id: string;
 }
--- src/modules/mo-service-library/data-transfer-objects/service/service-patch-request.dto.ts
+++ src/modules/mo-service-library/data-transfer-objects/service/service-patch-request.dto.ts
@@ -1,4 +1,11 @@
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { ServiceCreateRequestDto } from './service-create-request.dto';
 
 export class ServicePatchRequestDto extends ServiceCreateRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  @Expose()
+  id: string;
 }
