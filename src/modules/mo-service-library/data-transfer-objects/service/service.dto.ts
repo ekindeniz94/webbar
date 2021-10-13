8f8ea30956852f6b5239fae5b498a2d421d8fc9a
Daniel <daniel@mogenius.com>
2021-10-13 16:41:41 +0200

    fix: service Dto.

--- src/modules/mo-service-library/data-transfer-objects/service/service.dto.ts
+++ src/modules/mo-service-library/data-transfer-objects/service/service.dto.ts
@@ -1,11 +1,15 @@
 import { Expose, Type } from 'class-transformer';
 import { BaseEntityDto } from '../../../mo-core';
-import { NamespaceServiceEnvVarDto } from '../../../mo-namespace/data-transfer-objects/namespace-service';
 import { FilePublicDto } from '../../../mo-file';
+import { NamespaceServiceEnvVarDto } from '../../../mo-namespace/data-transfer-objects/namespace-service';
+import { ServiceTypeEnum } from '../../enums';
 
 export class ServiceDto extends BaseEntityDto {
   @Expose()
   name: string;
+
+  @Expose()
+  serviceType: ServiceTypeEnum;
 
   @Expose()
   description: string;
