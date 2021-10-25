e71c9098be437d08c077158b6865191be9d744da
Behrang Alavi <behrang@mogenius.com>
2021-10-25 10:09:21 +0200

    fix: add notificationState namespace notification

--- src/modules/mo-notification/data-transfer-objects/namespace-notification.dto.ts
+++ src/modules/mo-notification/data-transfer-objects/namespace-notification.dto.ts
@@ -1,6 +1,7 @@
 import { NotificationDto } from './notification.dto';
 import { Expose, Transform, Type } from 'class-transformer';
 import { NamespaceDto, NamespaceServiceDto } from '../../mo-namespace';
+import { K8sNotificationStateEnum } from '../enums';
 
 export class NamespaceNotificationDto extends NotificationDto {
   @Type(() => NamespaceDto)
@@ -17,21 +18,7 @@ export class NamespaceNotificationDto extends NotificationDto {
 
   @Expose()
   commandId: string;
+
+  @Expose()
+  notificationState: K8sNotificationStateEnum;
 }
-
-/*
-
-"id": "030ede8c-c288-4010-ba13-4d2acaeeb003",
-            "createdAt": "2021-10-20T05:34:13.995Z",
-            "title": "NamespaceDelete -> testbeh-prod-c6n50s",
-            "content": "null",
-            "type": "info",
-            "contentType": "TEXT",
-            "userId": "c1a87d09-ae2a-4f72-9108-4da480c85541",
-            "namespaceId": null,
-            "namespaceServiceId": null,
-            "subNotificationCount": null,
-            "namespaceName": null,
-            "namespaceServiceName": null,
-            "commandId": "3rr2mmayr6s6pd7wuganfq6"
- */
