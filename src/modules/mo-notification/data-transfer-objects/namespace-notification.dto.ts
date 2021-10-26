5f20db33342f5d1ef05cf402685525d26959d417
Behrang Alavi <behrang@mogenius.com>
2021-10-26 10:59:14 +0200

    fix: add stage to notification dto

--- src/modules/mo-notification/data-transfer-objects/namespace-notification.dto.ts
+++ src/modules/mo-notification/data-transfer-objects/namespace-notification.dto.ts
@@ -1,6 +1,6 @@
 import { NotificationDto } from './notification.dto';
 import { Expose, Transform, Type } from 'class-transformer';
-import { NamespaceDto, NamespaceServiceDto } from '../../mo-namespace';
+import { NamespaceDto, NamespaceServiceDto, NamespaceStageDto } from '../../mo-namespace';
 import { K8sNotificationStateEnum } from '../enums';
 
 export class NamespaceNotificationDto extends NotificationDto {
@@ -11,6 +11,10 @@ export class NamespaceNotificationDto extends NotificationDto {
   @Type(() => NamespaceServiceDto)
   @Expose()
   namespaceService?: NamespaceServiceDto;
+
+  @Type(() => NamespaceStageDto)
+  @Expose()
+  namespaceStage?: NamespaceStageDto;
 
   @Transform(({ value }) => +value)
   @Expose()
