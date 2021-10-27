1bd8ea020f9cd788941d695172b42aaae5e220e5
mogenius git-user <git@mogenius.com>
2021-10-27 10:58:56 +0200

    fix: many minor improvements.

--- src/modules/mo-namespace/data-transfer-objects/namespace/namespace-patch-request.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace/namespace-patch-request.dto.ts
@@ -1,19 +1,7 @@
 import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
-import { DTO_VALIDATION_CONST } from '../../../mo-core';
+import { IsOptional } from 'class-validator';
 
 export class NamespacePatchRequestDto {
-  @IsNotEmpty()
-  @IsString()
-  @MinLength(DTO_VALIDATION_CONST.NAMESPACE.NAME.MIN)
-  @MaxLength(DTO_VALIDATION_CONST.NAMESPACE.NAME.MAX)
-  @Expose()
-  name: string;
-
-  @IsOptional()
-  @Expose()
-  users: string[];
-
   @IsOptional()
   @Expose()
   icon: string;
--- src/modules/mo-namespace/data-transfer-objects/stats/stats.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/stats/stats.dto.ts
@@ -1,4 +1,5 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
+import { isNumber } from 'lodash';
 import { PodStatsUnitEnum } from '../../enums';
 
 export class StatsDto {
@@ -6,11 +7,14 @@ export class StatsDto {
   unit: PodStatsUnitEnum;
 
   @Expose()
+  @Transform(({ value }) => (value && isNumber(value) ? Number(value.toFixed(2)) : 0))
   current: number;
 
   @Expose()
+  @Transform(({ value }) => (value && isNumber(value) ? value : 0))
   limit: number;
 
   @Expose()
+  @Transform(({ value }) => (value && isNumber(value) ? Math.ceil(value * 100) : 0))
   percentage: number;
 }
