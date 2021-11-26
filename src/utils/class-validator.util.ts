171db0754a7b90c21a8c07aeac51adff206d2f4c
Behrang Alavi <behrang@mogenius.com>
2021-11-26 15:22:52 +0100

    fix: update class-transformer, class-validator

--- src/utils/class-validator.util.ts
+++ src/utils/class-validator.util.ts
@@ -1,9 +1,9 @@
-import { classToPlain, plainToClass } from 'class-transformer';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { isEmpty } from 'class-validator';
 
 export const ValidateIfObjectNotEmpty = (obj: any, objectClass: any): boolean => {
   let result = false;
-  const data = classToPlain(plainToClass(objectClass, obj, { excludeExtraneousValues: true }));
+  const data = instanceToPlain(plainToInstance(objectClass, obj, { excludeExtraneousValues: true }));
   for (const key in data) {
     if (!isEmpty(data[key])) {
       result = true;
