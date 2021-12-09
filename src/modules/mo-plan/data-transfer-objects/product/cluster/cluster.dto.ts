73cf62cfa879b2571fee3d76c4583f3a662ef98d
mogenius git-user <git@mogenius.com>
2021-12-09 11:48:56 +0100

    fix: added geocoordinates to cluster dto.

--- src/modules/mo-plan/data-transfer-objects/product/cluster/cluster.dto.ts
+++ src/modules/mo-plan/data-transfer-objects/product/cluster/cluster.dto.ts
@@ -1,9 +1,14 @@
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import { BaseEntityDto } from '../../../../mo-core/data-transfer-objects/base.entity.dto';
+import { GeoCoordinatesDto } from '../../../../mo-core/data-transfer-objects/geo-coordinates.dto';
 
 export class ClusterDto extends BaseEntityDto  {
   @Expose()
   region: string;
+  
+  @Expose()
+  @Type(() => GeoCoordinatesDto)
+  geoLocation: GeoCoordinatesDto;
 
   @Expose()
   name: string;
