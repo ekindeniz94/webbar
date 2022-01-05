bfb182a01491ab5c6250494c3a9cda3f47113ee4
Behrang Alavi <behrang@mogenius.com>
2022-01-05 16:09:34 +0100

    fix: NamespacePublicDto display default value

--- src/modules/mo-namespace/data-transfer-objects/namespace/namespace-public.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace/namespace-public.dto.ts
@@ -1,4 +1,4 @@
-import { Expose, Type } from 'class-transformer';
+import {Expose, Transform, Type} from 'class-transformer';
 import { BaseEntityDto } from '../../../mo-core';
 import { SubscriptionDto } from '../../../mo-subscription-pool';
 import { NamespaceStateEnum } from '../../enums';
@@ -6,11 +6,13 @@ import { NamespaceServiceDto } from '../namespace-service';
 import { NamespaceStageDto } from '../namespace-stage';
 import { NamespaceKeypairPublicDto } from './namespace-keypair-public.dto';
 import { GitConnectionDto } from '../../../mo-git';
+import {isString} from "class-validator";
 
 export class NamespacePublicDto extends BaseEntityDto {
   @Expose()
   shortId: string;
 
+  @Transform(({ value, obj }) => (value && isString(value) && value.length > 0 ? value : obj.name))
   @Expose()
   displayName: string;
 
