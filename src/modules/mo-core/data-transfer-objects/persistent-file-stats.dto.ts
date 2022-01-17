a12fc366905f51922d25e7a51a9a0c33384b28d2
mogenius git-user <git@mogenius.com>
2022-01-17 13:48:47 +0100

    fix: improved nfs stats.

---
+++ src/modules/mo-core/data-transfer-objects/persistent-file-stats.dto.ts
@@ -0,0 +1,14 @@
+// { directories: {}, sum: 0, durationMs: 0 }
+
+import { Expose } from "class-transformer";
+
+export class PersistentFileStatsDto {
+    @Expose()
+    objects: Object;
+  
+    @Expose()
+    sum: number;
+  
+    @Expose()
+    durationMs?: number;
+  }
