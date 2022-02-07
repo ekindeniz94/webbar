739d299565b69ca382003abdb7e651d03dd01226
mogenius git-user <git@mogenius.com>
2022-02-07 15:41:18 +0100

    fix: removed services from namespace dto.

--- src/modules/mo-namespace/data-transfer-objects/namespace/namespace.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace/namespace.dto.ts
@@ -1,11 +1,10 @@
 import { Expose, Transform, Type } from 'class-transformer';
-import { BaseEntityDto } from '../../../mo-core';
-import { NamespaceStateEnum } from '../../enums';
-import { NamespaceServiceDto } from '../namespace-service';
-import { NamespaceKeypairDto } from './namespace-keypair.dto';
-import { GitConnectionDto } from '../../../mo-git';
 import { isString } from 'class-validator';
+import { BaseEntityDto } from '../../../mo-core';
+import { GitConnectionDto } from '../../../mo-git';
 import { SubscriptionDto } from '../../../mo-payment';
+import { NamespaceStateEnum } from '../../enums';
+import { NamespaceKeypairDto } from './namespace-keypair.dto';
 
 export class NamespaceDto extends BaseEntityDto {
   @Expose()
@@ -34,10 +33,6 @@ export class NamespaceDto extends BaseEntityDto {
   @Expose()
   description: string;
 
-  @Type(() => NamespaceServiceDto)
-  @Expose()
-  services: NamespaceServiceDto[];
-
   @Expose()
   state: NamespaceStateEnum;
 
@@ -47,10 +42,6 @@ export class NamespaceDto extends BaseEntityDto {
   @Type(() => SubscriptionDto)
   @Expose()
   subscription: SubscriptionDto;
-
-  // @Type(() => NamespaceStageDto)
-  // @Expose()
-  // stages: NamespaceStageDto[];
 
   @Type(() => GitConnectionDto)
   @Expose()
