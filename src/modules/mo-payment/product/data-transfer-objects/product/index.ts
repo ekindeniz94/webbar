b26bee9d231c0a78f2dbd08027a05a992f7468f3
Daniel <daniel@mogenius.com>
2022-02-07 11:45:47 +0100

    fix: added homepage-product-dto

--- src/modules/mo-payment/product/data-transfer-objects/product/index.ts
+++ src/modules/mo-payment/product/data-transfer-objects/product/index.ts
@@ -3,3 +3,4 @@ export * from './product-bullet-point.dto';
 export * from './product-create-request.dto';
 export * from './product-patch-request.dto';
 export * from './product-public.dto';
+export * from './product-homepage.dto';
---
+++ src/modules/mo-payment/product/data-transfer-objects/product/product-homepage.dto.ts
@@ -0,0 +1,13 @@
+import { Expose, Type } from 'class-transformer';
+import { BaseEntityDto, CountryDto } from '../../../../mo-core';
+import { ProductDto } from './product.dto';
+
+export class ProductHomepageDto extends BaseEntityDto {
+  @Type(() => ProductDto)
+  @Expose()
+  products: ProductDto[];
+
+  @Type(() => CountryDto)
+  @Expose()
+  country: CountryDto[];
+}
