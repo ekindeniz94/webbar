b3dccc4bae4fa4898f9d6a9aca89f5708fd7e43e
mogenius git-user <git@mogenius.com>
2022-02-21 13:54:24 +0100

    fix: added azure build log dto.

--- src/modules/mo-namespace/data-transfer-objects/namespace/index.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace/index.ts
@@ -1,5 +1,5 @@
 export * from './add-user-to-namespace-group-request.dto';
-export * from './namespace.dto';
+export * from './azure-build-log.dto';
 export * from './namespace-create-request.dto';
 export * from './namespace-keypair.dto';
 export * from './namespace-kubernetes-dashboard.dto';
@@ -7,5 +7,7 @@ export * from './namespace-list-item.dto';
 export * from './namespace-patch-request.dto';
 export * from './namespace-search-response.dto';
 export * from './namespace-user.dto';
+export * from './namespace.dto';
 export * from './remove-user-from-namespace-group-request.dto';
 export * from './upgrade-namespace-subscription-request.dto';
+
