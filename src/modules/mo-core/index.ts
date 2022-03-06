492d53abd780f1ec90e6cde8db3e3222920e0d49
Behrang Alavi <behrang@mogenius.com>
2022-03-06 19:36:37 +0100

    fix: add generateCorrelationId

--- src/modules/mo-core/index.ts
+++ src/modules/mo-core/index.ts
@@ -1,6 +1,12 @@
+import { MoUtils } from '../../utils/mo-utils';
+
 export * from './address';
 export * from './constantes';
 export * from './country';
 export * from './data-transfer-objects';
 export * from './enums';
 export * from './validation-decorators';
+
+export const generateCorrelationId = (): string => {
+  return `mo_${MoUtils.nanoid()}`;
+};
