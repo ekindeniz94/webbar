e852fdcca1b4c82d09f84b9aeb2a7a5ab1daa31c
Behrang Alavi <behrang@mogenius.com>
2022-03-08 13:19:37 +0100

    fix: k8s IResources

--- src/modules/mo-namespace/interfaces/index.ts
+++ src/modules/mo-namespace/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './docker-template-environment-variable';
 export * from './managed-docker-service';
 export * from './managed-docker-template';
 export * from './available-resources.interface';
+export * from './resources';
---
+++ src/modules/mo-namespace/interfaces/resources.ts
@@ -0,0 +1,5 @@
+export interface IResources {
+  cpuCore: number;
+  memory: number;
+  temporaryStorage: number;
+}
