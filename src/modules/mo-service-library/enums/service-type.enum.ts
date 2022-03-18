ff0130d4affe0b580d1561648d3b7a66f1f7d66d
mogenius git-user <git@mogenius.com>
2022-03-18 20:33:35 +0100

    fix: service types extended.

--- src/modules/mo-service-library/enums/service-type.enum.ts
+++ src/modules/mo-service-library/enums/service-type.enum.ts
@@ -1,6 +1,11 @@
 export enum ServiceTypeEnum {
   DOCKER = 'DOCKER',
-  DOCKER_TEMPLATE = 'DOCKER_TEMPLATE'
+  DOCKER_TEMPLATE = 'DOCKER_TEMPLATE',
+  DOCKER_COMPOSE_TEMPLATE = 'DOCKER_COMPOSE_TEMPLATE'
 }
 
-export const ServiceTypeEnum_ALL = [ServiceTypeEnum.DOCKER, ServiceTypeEnum.DOCKER_TEMPLATE];
+export const ServiceTypeEnum_ALL = [
+  ServiceTypeEnum.DOCKER,
+  ServiceTypeEnum.DOCKER_TEMPLATE,
+  ServiceTypeEnum.DOCKER_COMPOSE_TEMPLATE
+];
