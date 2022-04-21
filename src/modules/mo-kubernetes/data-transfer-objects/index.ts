0eb810fffbceee37547c3d9169b270ac88b11cd0
Behrang Alavi <behrang@mogenius.com>
2022-04-21 09:51:21 +0200

    fix: index export issue

--- src/modules/mo-kubernetes/data-transfer-objects/index.ts
+++ src/modules/mo-kubernetes/data-transfer-objects/index.ts
@@ -3,7 +3,7 @@ export * from './kubernetes-event-involved-object.dto';
 export * from './kubernetes-event-metadata.dto';
 export * from './kubernetes-event-source.dto';
 
-export * from './kubernetes-event-meta.dto';
-export * from './kubernetes-event-status.dto';
+// export * from './kubernetes-event-meta.dto';
+// export * from './kubernetes-event-status.dto';
 export * from './kubernetes-event.dto';
 export * from './kubernetes-public-event.dto';
