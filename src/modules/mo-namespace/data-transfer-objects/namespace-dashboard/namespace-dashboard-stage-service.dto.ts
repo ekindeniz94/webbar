523acdfcff3b9b5b38dd50c47a621f8baa3ce89a
Behrang Alavi <behrang@mogenius.com>
2022-05-06 13:20:31 +0200

    fix: add state to NamespaceDashboardStageServiceDto

--- src/modules/mo-namespace/data-transfer-objects/namespace-dashboard/namespace-dashboard-stage-service.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace-dashboard/namespace-dashboard-stage-service.dto.ts
@@ -2,6 +2,7 @@ import { Expose, Transform, Type } from 'class-transformer';
 import { NamespaceDashboardStageServiceLibraryDto } from './namespace-dashboard-stage-service-library.dto';
 import { NamespaceDashboardStageServiceNotificationDto } from './namespace-dashboard-stage-service-notification.dto';
 import { isArray } from 'class-validator';
+import { NamespaceServiceStateEnum } from '../../enums';
 
 export class NamespaceDashboardStageServiceDto {
   @Expose()
@@ -15,6 +16,9 @@ export class NamespaceDashboardStageServiceDto {
 
   @Expose()
   displayName: string;
+
+  @Expose()
+  state: NamespaceServiceStateEnum;
 
   @Type(() => NamespaceDashboardStageServiceNotificationDto)
   @Transform(({ value }) => (value && isArray(value) ? value : []))
