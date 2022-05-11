4b534e29d9b50f950fde35fbd7032ffa2927c988
Behrang Alavi <behrang@mogenius.com>
2022-05-11 12:25:18 +0200

    fix: parse CloudflareLogHttpDto values (to number)

--- src/modules/mo-cloudflare/data-transfer-objects/cloudflare-log-http.dto.ts
+++ src/modules/mo-cloudflare/data-transfer-objects/cloudflare-log-http.dto.ts
@@ -1,24 +1,31 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
+import { isNumber } from 'class-validator';
 
 export class CloudflareLogHttpDto {
   @Expose()
   clientRequestHost: string;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   clientRequestBytes: number;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   cacheResponseBytes: number;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   originResponseStatus: number;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   originResponseBytes: number;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   edgeResponseBytes: number;
 
+  @Transform(({ value }) => (isNumber(+value) && !isNaN(+value) ? +value : 0))
   @Expose()
   requestCount: number;
 }
