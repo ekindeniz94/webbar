154ac6479cd2fc64b7b3e457daae9b4eb976675a
Behrang Alavi <behrang@mogenius.com>
2022-05-17 14:53:09 +0200

    fix: AppTagDto

--- src/modules/mo-app-library/data-transfer-objects/app-tag.dto.ts
+++ src/modules/mo-app-library/data-transfer-objects/app-tag.dto.ts
@@ -2,10 +2,16 @@ import { Expose, Transform, Type } from 'class-transformer';
 import { isBoolean, IsString } from 'class-validator';
 import { BaseEntityDto } from '../../mo-core';
 
-export class AppTagDto extends BaseEntityDto {
+export class AppTagDto {
   @IsString()
   @Expose()
   name: string;
+
+  @Expose()
+  createdAt: string | Date;
+
+  @Expose()
+  updatedAt: string | Date;
 
   @Type(() => Boolean)
   @Transform(({ value }) => (isBoolean(value) ? value : true))
