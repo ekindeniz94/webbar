13d7c78cfb18fddbfc7c87a432ca5d36d8e3d95f
Behrang Alavi <behrang@mogenius.com>
2022-06-08 15:55:25 +0200

    fix: base entity dto add date transform to json

--- src/modules/mo-core/data-transfer-objects/base.entity.dto.ts
+++ src/modules/mo-core/data-transfer-objects/base.entity.dto.ts
@@ -1,5 +1,6 @@
-import { Expose, Type } from 'class-transformer';
+import { Expose, Transform, Type } from 'class-transformer';
 import { UserPublicDto } from '../../mo-user/data-transfer-objects/user/user-public.dto';
+import moment from 'moment';
 
 export abstract class BaseEntityDto {
   @Expose()
@@ -9,11 +10,11 @@ export abstract class BaseEntityDto {
   @Expose()
   createdBy: UserPublicDto;
 
-  // TODO
+  @Transform(({ value }) => (value ? moment(value).toJSON() : value))
   @Expose()
   createdAt: string | Date;
 
-  // TODO
+  @Transform(({ value }) => (value ? moment(value).toJSON() : value))
   @Expose()
   updatedAt: string | Date;
 }
