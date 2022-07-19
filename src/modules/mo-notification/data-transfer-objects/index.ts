789eda107a1abc3c78d45ffe6954cefb1e5f16e0
Behrang Alavi <behrang@mogenius.com>
2022-07-19 12:31:19 +0200

    fix: add K8sManagerTrafficMessageDto

--- src/modules/mo-notification/data-transfer-objects/index.ts
+++ src/modules/mo-notification/data-transfer-objects/index.ts
@@ -1,4 +1,5 @@
 export * from './k8s-manager-job';
+export * from './k8s-manager-traffic-message.dto';
 
 export * from './k8s-notification.dto';
 export * from './notification.dto';
---
+++ src/modules/mo-notification/data-transfer-objects/k8s-manager-traffic-message.dto.ts
@@ -0,0 +1,10 @@
+import { Expose } from 'class-transformer';
+import { BuildStateEnum, DeploymentStateEnum, K8sNotificationStateEnum } from '../enums';
+
+export class K8sManagerTrafficMessageDto {
+  @Expose()
+  podName: string;
+
+  @Expose()
+  value: string;
+}
