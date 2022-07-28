b6264de037726b8e91ae381d39ccc9e413c41b81
Benedikt Iltisberger <bene@mogenius.com>
2022-07-28 10:27:22 +0200

    fix: added NamespaceServiceDockerComposeResponseDto.

--- src/modules/mo-namespace/data-transfer-objects/namespace-service/namespace-service-docker-compose-response.dto.ts
+++ src/modules/mo-namespace/data-transfer-objects/namespace-service/namespace-service-docker-compose-response.dto.ts
@@ -1,12 +1,11 @@
-import { Expose } from "class-transformer";
-import { AppDto, AppIdDto } from "../../../mo-app-library";
-import { NamespaceServiceCreateRequestDto } from "./namespace-service-create-request.dto";
+import { Expose } from 'class-transformer';
+import { AppDto, AppIdDto } from '../../../mo-app-library';
+import { NamespaceServiceCreateRequestDto } from './namespace-service-create-request.dto';
 
 export class NamespaceServiceDockerComposeResponseDto {
+  @Expose()
+  services: NamespaceServiceCreateRequestDto[];
 
-    @Expose()
-    services: NamespaceServiceCreateRequestDto[];
-
-    @Expose()
-    apps: Map<AppIdDto, AppDto>;
+  @Expose()
+  apps: { id: AppIdDto; app: AppDto }[];
 }
