480d346fd6101926e6e6f29818d4963912fb278a
Andreas Eri <andreas@mogenius.com>
2022-08-11 15:56:44 +0200

    add grant_type implicit for customer support mode

--- src/modules/mo-user/types/auth-token-grants.ts
+++ src/modules/mo-user/types/auth-token-grants.ts
@@ -1,1 +1,1 @@
-export type AuthTokenGrants = 'authorization_code' | 'client_credentials' | 'refresh_token' | 'password';
+export type AuthTokenGrants = 'authorization_code' | 'client_credentials' | 'refresh_token' | 'password' | 'implicit';
