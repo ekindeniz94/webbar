7bf1d7b104f4ed9dfbed6564290774263603e751
Andreas Eri <andreas@mogenius.com>
2023-01-05 16:03:31 +0100

    fix: url decoding, min length

--- src/modules/mo-core/data-transfer-objects/user-application-request.dto.ts
+++ src/modules/mo-core/data-transfer-objects/user-application-request.dto.ts
@@ -1,7 +1,7 @@
 import { Expose } from 'class-transformer';
-import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+import { IsString, IsOptional, IsNotEmpty, MinLength } from 'class-validator';
 
 export class UserApplicationDto {
   @Expose() @IsString() @IsOptional() interests: string;
-  @Expose() @IsString() @IsNotEmpty() description: string;
+  @Expose() @IsString() @IsNotEmpty() @MinLength(128) description: string;
 }
