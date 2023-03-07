397254ccdb5aae383b11a695a919b17f6eff29b2
Behrang Alavi <behrang@mogenius.com>
2023-03-07 22:01:56 +0100

    fix: update patch cluster

--- src/modules/mo-payment/product/data-transfer-objects/cluster/cluster-patch-request.dto.ts
+++ src/modules/mo-payment/product/data-transfer-objects/cluster/cluster-patch-request.dto.ts
@@ -1,5 +1,5 @@
 import { ClusterCreateRequestDto } from './cluster-create-request.dto';
-import { IsBoolean, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 import { Expose, Transform } from 'class-transformer';
 import { MoUtils } from '@mo/js-utils';
 
@@ -10,12 +10,13 @@ export class ClusterPatchRequestDto extends ClusterCreateRequestDto {
   @Expose()
   id: string;
 
+  @IsOptional()
   @IsString()
   @Expose()
   clusterId: string;
 
   @Transform(({ value }) => MoUtils.parseBoolean(value))
-  @IsNotEmpty()
+  @IsOptional()
   @IsBoolean()
   @Expose()
   apiKeyIsActive: boolean;
