eadb1be379f846d4febc40460189d42a62fbc0da
Behrang Alavi <behrang@mogenius.com>
2023-03-17 15:04:29 +0100

    fix: k8s dtos

--- src/modules/mo-core/data-transfer-objects/k8s/k8s-stage.dto.ts
+++ src/modules/mo-core/data-transfer-objects/k8s/k8s-stage.dto.ts
@@ -1,26 +1,28 @@
-import { Expose, Type } from 'class-transformer';
+import { Expose, Transform, Type } from 'class-transformer';
 import { K8sServiceDto } from './k8s-service.dto';
+import { isArray } from 'class-validator';
 
 export class K8sStageDto {
-    @Expose()
-    id: string;
+  @Expose()
+  id: string;
 
-    @Expose()
-    displayName: string;
+  @Expose()
+  displayName: string;
 
-    @Expose()
-    k8sName: string; // MoNamespaceUtils.kubernetesName(namespace, stage)
+  @Expose()
+  k8sName: string; // MoNamespaceUtils.kubernetesName(namespace, stage)
 
-    @Expose()
-    hostname: string; // MoNamespaceUtils.fullHostname(namespace, stage)
+  @Expose()
+  hostname: string; // MoNamespaceUtils.fullHostname(namespace, stage)
 
-    @Expose()
-    storageSizeInMb: number; // namespace.subscription.plan.product.persistentDiskInMb
+  @Expose()
+  storageSizeInMb: number; // namespace.subscription.plan.product.persistentDiskInMb
 
-    @Type(() => K8sServiceDto)
-    @Expose()
-    services: K8sServiceDto[];
+  @Type(() => K8sServiceDto)
+  @Transform(({ value }) => (value && isArray(value) ? value : []))
+  @Expose()
+  services: K8sServiceDto[];
 
-    @Expose()
-    cloudflareProxied: boolean;
+  @Expose()
+  cloudflareProxied: boolean;
 }
