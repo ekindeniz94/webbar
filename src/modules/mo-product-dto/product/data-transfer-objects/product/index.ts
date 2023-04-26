f7717aa96e07238c27ffc90f2ff309e0505e6211
MoFranky <franky@mogenius.com>
2023-04-26 11:33:44 +0200

    feat: import.

--- src/modules/mo-product-dto/product/data-transfer-objects/product/index.ts
+++ src/modules/mo-product-dto/product/data-transfer-objects/product/index.ts
@@ -5,3 +5,4 @@ export * from './product-bullet-point.dto';
 export * from './product-create-request.dto';
 export * from './product-patch-request.dto';
 export * from './product-public.dto';
+export * from './product-resources-response.dto';
