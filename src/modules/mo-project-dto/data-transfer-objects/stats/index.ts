e21e594fc4151630241e7631d8ab0716ec124ee5
Behrang Alavi <behrang@mogenius.com>
2023-05-04 22:38:08 +0200

    fix: service overview list

---
+++ src/modules/mo-project-dto/data-transfer-objects/stats/index.ts
@@ -0,0 +1,3 @@
+export * from './cpu.dto'
+export * from './memory.dto'
+export * from './ephemeralStorage.dto'
---
+++ src/modules/mo-project-dto/data-transfer-objects/stats/memory.dto.ts
@@ -0,0 +1,23 @@
+import { Expose, Transform, Type } from 'class-transformer';
+import { isNumber } from 'lodash';
+
+export class MemoryDto {
+  @Type(() => Number)
+  @Transform(({ value }) => value ?? 0)
+  @Expose()
+  current: number;
+
+  @Type(() => Number)
+  @Transform(({ value }) => value ?? 0)
+  @Expose()
+  limit: number;
+
+  get percentage(): number {
+    this.limit = this.limit ?? 0;
+    if (!this.limit) {
+      return 0;
+    }
+    const value = (this.current ?? 0) / this.limit;
+    return value && isNumber(value) ? +(Math.round(value * 100 * 100) / 100).toFixed(2) : 0;
+  }
+}
