33f5c792eb2c906bd67469dad087ff53c81fae32
Benedikt Iltisberger <bene@mogenius.com>
2023-05-31 16:19:33 +0200

    fix: added 2 new workloads.

--- src/modules/mo-product-dto/product/enums/workload/kubernetes-workload.enum.ts
+++ src/modules/mo-product-dto/product/enums/workload/kubernetes-workload.enum.ts
@@ -11,7 +11,10 @@ export enum KubernetesWorkloadEnum {
   STATEFUL_SET = 'stateful_set',
   JOB = 'job',
   CRON_JOB = 'cron_job',
-  REPLICA_SET = 'replica_set'
+  REPLICA_SET = 'replica_set',
+  PERSISTENT_VOLUME = 'persistent_volume',
+  PERSISTENT_VOLUME_CLAIM = 'persistent_volume_claim'
+
 }
 
 export const KubernetesWorkloadEnum_ALL = [
@@ -27,5 +30,7 @@ export const KubernetesWorkloadEnum_ALL = [
   KubernetesWorkloadEnum.STATEFUL_SET,
   KubernetesWorkloadEnum.JOB,
   KubernetesWorkloadEnum.CRON_JOB,
-  KubernetesWorkloadEnum.REPLICA_SET
+  KubernetesWorkloadEnum.REPLICA_SET,
+  KubernetesWorkloadEnum.PERSISTENT_VOLUME,
+  KubernetesWorkloadEnum.PERSISTENT_VOLUME_CLAIM
 ];
