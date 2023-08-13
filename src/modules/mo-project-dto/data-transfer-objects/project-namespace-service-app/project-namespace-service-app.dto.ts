8d2e90a8c014f4c746c0e7ad3fd0d7a8220457f1
Behrang Alavi <behrang@mogenius.com>
2023-08-13 21:56:06 +0200

    fix: update ProjectNamespaceServiceAppDto

--- src/modules/mo-project-dto/data-transfer-objects/project-namespace-service-app/project-namespace-service-app.dto.ts
+++ src/modules/mo-project-dto/data-transfer-objects/project-namespace-service-app/project-namespace-service-app.dto.ts
@@ -1,5 +1,6 @@
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 import { AppLibraryTypeEnum } from '../../../mo-app-library-dto';
+import { ProjectNamespaceServiceKubernetesSettingsDto } from '../project-namespace-service-kubernetes-settings';
 
 export class ProjectNamespaceServiceAppDto {
   @Expose()
@@ -19,4 +20,8 @@ export class ProjectNamespaceServiceAppDto {
 
   @Expose()
   description: string;
+
+  @Type(() => ProjectNamespaceServiceKubernetesSettingsDto)
+  @Expose()
+  kubernetesLimits: ProjectNamespaceServiceKubernetesSettingsDto;
 }
