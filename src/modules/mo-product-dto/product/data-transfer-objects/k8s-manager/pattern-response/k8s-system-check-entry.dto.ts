5b1db2ab7e19c4a8c417493f5275ee867b07a3f7
Behrang Alavi <behrang@mogenius.com>
2023-11-09 20:15:14 +0100

    fix: update K8sSystemCheckEntryDto

--- src/modules/mo-product-dto/product/data-transfer-objects/k8s-manager/pattern-response/k8s-system-check-entry.dto.ts
+++ src/modules/mo-product-dto/product/data-transfer-objects/k8s-manager/pattern-response/k8s-system-check-entry.dto.ts
@@ -1,16 +1,11 @@
 import { Expose, Type } from 'class-transformer';
 import { isEmpty } from 'class-validator';
+import { K8sSystemCheckEntryStatusEnum } from '../../../enums/k8s-manager/k8s-system-check-entry-status.enum';
 
 export class K8sSystemCheckEntryDto {
-  private _disabled: boolean = false;
-
   @Type(() => String)
   @Expose()
   checkName: string;
-
-  @Type(() => Boolean)
-  @Expose()
-  success: boolean;
 
   @Type(() => String)
   @Expose()
@@ -22,25 +17,12 @@ export class K8sSystemCheckEntryDto {
   @Expose()
   uninstallPattern: string;
 
+  @Expose()
+  status: K8sSystemCheckEntryStatusEnum;
+
   get isInfo(): boolean {
     return (
       !this.installPattern || isEmpty(this.installPattern) || !this.uninstallPattern || isEmpty(this.uninstallPattern)
     );
-  }
-
-  get install(): boolean {
-    return !this.success;
-  }
-
-  get uninstall(): boolean {
-    return this.success;
-  }
-
-  get disabled(): boolean {
-    return this._disabled;
-  }
-
-  set disabled(value: boolean) {
-    this._disabled = value;
   }
 }
