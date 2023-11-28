0b6bb69c631f10bc353cf4dd9513a77b9e1ec1ee
Behrang Alavi <behrang@mogenius.com>
2023-11-28 20:54:44 +0100

    fix: update KubernetesWorkloadListRequestDto

--- src/modules/mo-product-dto/product/data-transfer-objects/cluster/workload/kubernetes-workload-list.request.dto.ts
+++ src/modules/mo-product-dto/product/data-transfer-objects/cluster/workload/kubernetes-workload-list.request.dto.ts
@@ -1,9 +1,13 @@
 import { Expose } from 'class-transformer';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { KubernetesWorkloadEnum } from '../../../enums';
 
 export class KubernetesWorkloadListRequestDto {
   @IsEnum(KubernetesWorkloadEnum)
   @Expose()
   kubernetesWorkload: KubernetesWorkloadEnum;
+
+  @IsOptional()
+  @Expose()
+  namespace?: string;
 }
