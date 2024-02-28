1956944f377947f3b45e2781efa3489b5c42f2f0
Behrang Alavi <behrang@mogenius.com>
2024-02-28 21:08:16 +0100

    fix: rm FileListFullDto

--- src/modules/mo-product-dto/product/data-transfer-objects/file/file-list-full.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Expose, Type } from 'class-transformer';
-import { FileListDto } from './file-list.dto';
-
-export class FileListFullDto extends FileListDto {
-  @Expose()
-  @Type(() => FileListFullDto)
-  child?: FileListFullDto[];
-}
