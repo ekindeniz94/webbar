a886b1eb68a08affe725c26fe61f9a6cd947673e
Behrang Alavi <behrang@mogenius.com>
2024-04-22 13:48:11 +0200

    fix: K8sMsgTaskDto

--- src/modules/mo-notification/data-transfer-objects/k8s-manager-job/k8s-msg-task.dto.ts
+++ src/modules/mo-notification/data-transfer-objects/k8s-manager-job/k8s-msg-task.dto.ts
@@ -1,10 +1,11 @@
 import { K8sMsgDto } from './k8s-msg.dto';
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 
 export class K8sMsgTaskDto {
   @Expose()
   id: string;
 
+  @Type(() => K8sMsgDto)
   @Expose()
   data: K8sMsgDto;
 }
