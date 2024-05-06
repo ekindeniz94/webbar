436f46fad4f80f3367022e73fb84f5e569c0c984
Andreas Eri <andreas@mogenius.com>
2024-05-06 16:44:34 +0200

    fix: add frontend context notification type enum to k8sjobdto

--- src/modules/mo-kubernetes/enums/k8s-manager/index.ts
+++ src/modules/mo-kubernetes/enums/k8s-manager/index.ts
@@ -1,3 +1,4 @@
 export * from './k8s-build-state.enum';
 export * from './k8s-job-state.enum';
 export * from './k8s-system-check-entry-status.enum';
+export * from './k8s-job-notification-type.enum';
---
+++ src/modules/mo-kubernetes/enums/k8s-manager/k8s-job-notification-type.enum.ts
@@ -0,0 +1,5 @@
+export enum K8sJobNotificationTypeEnum {
+  VOLUME = 'Volume',
+  BUILD = 'Build',
+  SERVICE = 'Service'
+}
