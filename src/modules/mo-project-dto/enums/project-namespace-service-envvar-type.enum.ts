5142efea763daa280e8fe82e2122bb77b0d5c587
Behrang Alavi <behrang@mogenius.com>
2024-05-29 15:04:40 +0200

    fix: ProjectNamespaceServiceEnvVarTypeEnum.PLAINTEXT

--- src/modules/mo-project-dto/enums/project-namespace-service-envvar-type.enum.ts
+++ src/modules/mo-project-dto/enums/project-namespace-service-envvar-type.enum.ts
@@ -2,7 +2,7 @@ export enum ProjectNamespaceServiceEnvVarTypeEnum {
   PLAINTEXT = 'PLAINTEXT',
   KEY_VAULT = 'KEY_VAULT',
   VOLUME_MOUNT = 'VOLUME_MOUNT',
-  VOLUME_MOUNT_SEED = 'VOLUME_MOUNT_SEED',
+  // VOLUME_MOUNT_SEED = 'VOLUME_MOUNT_SEED',
   // CHANGE_OWNER = 'CHANGE_OWNER',
   HOSTNAME = 'HOSTNAME'
 }
@@ -11,7 +11,7 @@ export const ProjectNamespaceServiceEnvVarTypeEnum_ALL = [
   ProjectNamespaceServiceEnvVarTypeEnum.PLAINTEXT,
   ProjectNamespaceServiceEnvVarTypeEnum.KEY_VAULT,
   ProjectNamespaceServiceEnvVarTypeEnum.VOLUME_MOUNT,
-  ProjectNamespaceServiceEnvVarTypeEnum.VOLUME_MOUNT_SEED,
+  // ProjectNamespaceServiceEnvVarTypeEnum.VOLUME_MOUNT_SEED,
   // ProjectNamespaceServiceEnvVarTypeEnum.CHANGE_OWNER,
   ProjectNamespaceServiceEnvVarTypeEnum.HOSTNAME
 ];
