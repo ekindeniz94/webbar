088133cd4127309cac78ea5b6f6b8ca3c7c69af7
mogenius git-user <git@mogenius.com>
2024-07-17 16:00:25 +0200

    fix: mv string literals to enums

--- src/modules/mo-project-dto/data-transfer-objects/project-namespace-service/project-namespace-service-list-cronjob-jobs-item.dto.ts
+++ src/modules/mo-project-dto/data-transfer-objects/project-namespace-service/project-namespace-service-list-cronjob-jobs-item.dto.ts
@@ -2,6 +2,10 @@ import { Expose, Transform, Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { ProjectNamespaceServiceListCronjobJobsItemStatusMessageDto } from './project-namespace-service-list-cronjob-jobs-item-status-message.dto';
 import moment from 'moment';
+import {
+  ProjectNamespaceServiceListCronjobJobStatusEnum,
+  ProjectNamespaceServiceListCronjobJobTileStatusEnum
+} from 'src/mo-core-base';
 
 export class ProjectNamespaceServiceListCronjobJobsItemDto {
   @IsNotEmpty()
@@ -11,16 +15,13 @@ export class ProjectNamespaceServiceListCronjobJobsItemDto {
   @Expose()
   schedule: Date;
 
-  @IsString()
   @IsNotEmpty()
   @Expose()
-  status: 'Active' | 'Succeeded' | 'Failed' | 'Suspended' | 'Unknown';
+  status: ProjectNamespaceServiceListCronjobJobStatusEnum;
 
-  @IsString()
   @IsNotEmpty()
-  @Type(() => String)
   @Expose()
-  tileType: 'Job' | 'Empty';
+  tileType: ProjectNamespaceServiceListCronjobJobTileStatusEnum;
 
   @IsOptional()
   @IsString()
---
+++ src/modules/mo-project-dto/enums/project-namespace-service-list-cronjob-job-tile-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ProjectNamespaceServiceListCronjobJobTileStatusEnum {
+  Job = 'Job',
+  Empty = 'Empty'
+}
