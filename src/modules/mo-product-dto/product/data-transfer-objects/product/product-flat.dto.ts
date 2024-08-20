ad1358cfa6637b1ceeaeec2d5a888c53315231de
Behrang Alavi <behrang@mogenius.com>
2024-08-20 17:21:15 +0200

    fix: product max cluster

--- src/modules/mo-product-dto/product/data-transfer-objects/product/product-flat.dto.ts
+++ src/modules/mo-product-dto/product/data-transfer-objects/product/product-flat.dto.ts
@@ -49,9 +49,11 @@ export class ProductFlatDto extends BaseEntityDto {
   @Expose()
   persistentDiskInMb: number;
 
+  @Transform(({ value }) => (value > 0 ? value : 0))
   @Expose()
   clusterCountMax?: number;
 
+  @Transform(({ value }) => (value > 0 ? value : 0))
   @Expose()
   projectCountMax?: number;
 
