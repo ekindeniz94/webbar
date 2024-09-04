a9894b794b39762559143acf179d6547fa02bfd4
Behrang Alavi <behrang@mogenius.com>
2024-09-04 22:11:33 +0200

    fix: update ExternalSecretStoreDto

--- src/modules/mo-project-dto/data-transfer-objects/external-secret-storage/store/external-secret-store.dto.ts
+++ src/modules/mo-project-dto/data-transfer-objects/external-secret-storage/store/external-secret-store.dto.ts
@@ -1,6 +1,11 @@
 import { Expose, Transform } from 'class-transformer';
+import moment from 'moment';
 
 export class ExternalSecretStoreDto {
+  @Transform(({ value }) => (value && value !== 'undefined' && value !== 'null' ? moment(value).toDate() : value))
+  @Expose()
+  createdAt: Date;
+
   @Expose()
   name: string;
 
@@ -21,4 +26,28 @@ export class ExternalSecretStoreDto {
   @Transform(({ value, obj }) => value ?? obj['vault-url'], { toClassOnly: true })
   @Expose()
   vaultURL: string;
+
+  @Transform(({ value }) => {
+    switch (value) {
+      case 'True':
+      case true:
+        return true;
+      case 'False':
+      case false:
+        return false;
+      default:
+        return 'Unknown';
+    }
+  })
+  @Expose()
+  status: boolean | 'Unknown';
+
+  @Expose()
+  message: string;
+
+  @Expose()
+  reason: string;
+
+  @Expose()
+  type: 'Deleted' | 'Ready';
 }
