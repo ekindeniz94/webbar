c2883fc02bb8adaa99dcf415fe5a282170b4865e
mogenius git-user <git@mogenius.com>
2024-09-06 15:02:00 +0200

    fix: typos

--- src/modules/mo-project-dto/enums/project-notification-settings-type.enum.ts
+++ src/modules/mo-project-dto/enums/project-notification-settings-type.enum.ts
@@ -1,14 +1,14 @@
 export enum ProjectNotificationSettingsTypeEnum {
-  /// service
+  /// services
   BuildError = 'BUILD_ERROR',
   ServiceStatusError = 'SERVICE_STATUS_ERROR',
   HealthCheckError = 'HEALTH_CHECK_ERROR',
   /// resources
   ProjectResourcesWarning = 'PROJECT_RESOURCES_WARNING',
   ServiceUsageWarning = 'SERVICE_USAGE_WARNING',
-  /// integration
+  /// integrations
   GitIntegrationTokenExpiredWarning = 'GIT_INTEGRATION_TOKEN_EXPIRED_WARNING',
   GitIntegrationTokenRefreshError = 'GIT_INTEGRATION_TOKEN_REFRESH_ERROR',
-  /// user
+  /// users
   UserJoinedActivity = 'USER_JOINED_ACTIVITY'
 }
