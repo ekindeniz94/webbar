df8e20b64ed591ec53cd37f37ca18ba7ca40a8c5
mogenius git-user <git@mogenius.com>
2024-09-06 11:24:33 +0200

    fix: extend project notificartion defaults

--- src/modules/mo-project-dto/enums/project-notification-settings-type.enum.ts
+++ src/modules/mo-project-dto/enums/project-notification-settings-type.enum.ts
@@ -1,10 +1,14 @@
 export enum ProjectNotificationSettingsTypeEnum {
   /// service
-  FailingBuild = 'failing-build',
-
+  BuildError = 'BUILD_ERROR',
+  ServiceStatusError = 'SERVICE_STATUS_ERROR',
+  HealthCheckError = 'HEALTH_CHECK_ERROR',
+  /// resources
+  ProjectResourcesWarning = 'PROJECT_RESOURCES_WARNING',
+  ServiceUsageWarning = 'SERVICE_USAGE_WARNING',
   /// integration
-  /// cluster
+  GitIntegrationTokenExpiredWarning = 'GIT_INTEGRATION_TOKEN_EXPIRED_WARNING',
+  GitIntegrationTokenRefreshError = 'GIT_INTEGRATION_TOKEN_REFRESH_ERROR',
   /// user
-  /// testing
-  Testing = 'testing'
+  UserJoinedActivity = 'USER_JOINED_ACTIVITY'
 }
