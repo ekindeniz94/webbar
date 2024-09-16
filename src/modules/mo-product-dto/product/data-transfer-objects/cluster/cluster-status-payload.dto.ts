0b7f8ad09cba02f165fd7f8bca1e90c95453a08f
mogenius git-user <git@mogenius.com>
2024-09-16 15:26:37 +0200

    feat: add cluster status dto

--- src/modules/mo-product-dto/product/data-transfer-objects/cluster/cluster-status-payload.dto.ts
+++ src/modules/mo-product-dto/product/data-transfer-objects/cluster/cluster-status-payload.dto.ts
@@ -1,26 +1,40 @@
 import { Expose, Transform, Type } from 'class-transformer';
 import { ClusterNodeDto } from './cluster-node.dto';
-import { isArray, isIP, IsString } from 'class-validator';
 import _ from 'lodash';
 import { BaseEntityDto, CountryDto } from '@mogenius/database-dto';
 import { ClusterProviderEnum } from '../../enums';
 
-export class ClusterStatusPayloadDto extends BaseEntityDto {
-  @IsString({ each: true })
-  @Transform(({ value }) => {
-    value = (value && isArray(value) ? _.uniq(value) : []) as string[];
-    return value
-      .map((item: string) => {
-        if (item === 'localhost') {
-          return '127.0.0.1';
-        }
-        return item;
-      })
-      .filter((item: string) => isIP(item));
-  })
+export class ClusterStatusPayloadDto  {
   @Expose()
-  loadBalancerExternalIps: string[];
-
+  clusterName: string;
+  
+  @Expose()
+  pods: number;
+  
+  @Expose()
+  podCpuUsageInMilliCores: number;
+  
+  @Expose()
+  podCpuLimitInMilliCores: number;
+  
+  @Expose()
+  podMemoryUsageInBytes: number;
+  
+  @Expose()
+  podMemoryLimitInBytes: number;
+  
+  @Expose()
+  ephemeralStorageLimitInBytes: number;
+  
+  @Expose()
+  currentTime: string;
+  
+  @Expose()
+  kubernetesVersion: string;
+  
+  @Expose()
+  platform: string;
+  
   @Type(() => ClusterNodeDto)
   @Expose()
   nodeStats: ClusterNodeDto[];
