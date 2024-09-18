e91d00ca4b5a354475ff3ebd2d0ba2e165e6a180
MoFranky <franky@mogenius.com>
2024-09-18 09:02:04 +0200

    fix: All Keyvault array.

--- src/modules/mo-project-dto/enums/project-namespace-service-envvar-vault-type.enum.ts
+++ src/modules/mo-project-dto/enums/project-namespace-service-envvar-vault-type.enum.ts
@@ -2,3 +2,8 @@ export enum ProjectNamespaceServiceEnvVarVaultTypeEnum {
   MOGENIUS_VAULT = 'MOGENIUS_VAULT',
   HASHICORP_EXTERNAL_VAULT = 'HASHICORP_EXTERNAL_VAULT'
 }
+
+export const ProjectNamespaceServiceEnvVarVaultTypeEnum_ALL: ProjectNamespaceServiceEnvVarVaultTypeEnum[] = [
+  ProjectNamespaceServiceEnvVarVaultTypeEnum.MOGENIUS_VAULT,
+  ProjectNamespaceServiceEnvVarVaultTypeEnum.HASHICORP_EXTERNAL_VAULT
+];
