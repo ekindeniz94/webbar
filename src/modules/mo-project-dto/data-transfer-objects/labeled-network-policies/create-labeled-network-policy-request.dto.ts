f24c55df3a6fb911f56c8fb8f1b44119af0b5c94
Valentin Savenko <valentin@mogenius.com>
2024-10-08 14:48:46 +0200

    chore: add validators

--- src/modules/mo-project-dto/data-transfer-objects/labeled-network-policies/create-labeled-network-policy-request.dto.ts
+++ src/modules/mo-project-dto/data-transfer-objects/labeled-network-policies/create-labeled-network-policy-request.dto.ts
@@ -1,5 +1,5 @@
-import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Expose, Type } from 'class-transformer';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { K8sLabeledNetworkPolicyDto } from './labeled-network-policies.dto';
 
 export class CreateLabeledNetworkPolicyRequestDto {
@@ -7,6 +7,10 @@ export class CreateLabeledNetworkPolicyRequestDto {
   @IsString()
   @Expose()
   namespaceName: string;
+  
+  @IsNotEmpty()
+  @Type(() => K8sLabeledNetworkPolicyDto)
+  @ValidateNested({ message: '$property must be an object' })
   @Expose()
   labeledNetworkPolicy: K8sLabeledNetworkPolicyDto;
 }
