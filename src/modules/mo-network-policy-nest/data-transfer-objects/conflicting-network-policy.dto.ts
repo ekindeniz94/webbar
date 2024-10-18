40725dd05b4faff95d72be24c682d00460080a7e
Behrang Alavi <behrang@mogenius.com>
2024-10-18 16:18:44 +0200

    fix: ConflictingNetworkPolicyDto

--- src/modules/mo-network-policy-nest/data-transfer-objects/conflicting-network-policy.dto.ts
+++ src/modules/mo-network-policy-nest/data-transfer-objects/conflicting-network-policy.dto.ts
@@ -1,25 +1,32 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
-import { Expose } from 'class-transformer';
-import { ServiceControllerEnum } from '../../mo-project-dto';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
 import { V1NetworkPolicySpec } from '@kubernetes/client-node';
+import * as JSYAML from 'js-yaml';
 
 export class ConflictingNetworkPolicyDto {
   @IsNotEmpty()
   @IsString()
   @Expose()
-  namespaceName: string;
+  name: string;
 
   @IsNotEmpty()
   @IsString()
   @Expose()
-  controllerName: string;
+  namespaceName: string;
+
+  // @IsNotEmpty()
+  // @IsEnum(ServiceControllerEnum)
+  // @Expose()
+  // controllerType: ServiceControllerEnum;
 
   @IsNotEmpty()
-  @IsEnum(ServiceControllerEnum)
-  @Expose()
-  controllerType: ServiceControllerEnum;
-
-  @IsNotEmpty()
+  @Transform(({ value, obj }: { value: string; obj: ConflictingNetworkPolicyDto }) => {
+    obj.specYaML = JSYAML.dump(value);
+    return value;
+  })
   @Expose()
   spec: V1NetworkPolicySpec;
+
+  @Expose()
+  specYaML: string;
 }
