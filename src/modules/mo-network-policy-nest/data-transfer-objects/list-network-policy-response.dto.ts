bbcf7b825774baad55d8712cec5a0a0bbc9bf27c
eriandreas <andreas@mogenius.com>
2024-11-04 23:07:12 +0100

    fix: refactor np response dtos

--- src/modules/mo-network-policy-nest/data-transfer-objects/list-network-policy-response.dto.ts
+++ src/modules/mo-network-policy-nest/data-transfer-objects/list-network-policy-response.dto.ts
@@ -1,12 +1,10 @@
-import { Expose, Transform, Type } from 'class-transformer';
-import { IsArray } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
 import { ListNetworkPolicyNamespaceDto } from './list-network-policy-namespace.dto';
 import { K8sMessageResponseBaseDto } from '../../../modules/mo-kubernetes/data-transfer-objects/k8s-message-response-base.dto';
+import { MoUtils } from '@mogenius/js-utils';
 
 export class ListNetworkPolicyResponseDto extends K8sMessageResponseBaseDto<ListNetworkPolicyNamespaceDto[]> {
-  @IsArray()
-  @Transform((value) => value ?? [])
-  @Type(() => ListNetworkPolicyNamespaceDto)
+  @Transform(({ value }) => MoUtils.transformToDtoList(ListNetworkPolicyNamespaceDto, value))
   @Expose()
   data: ListNetworkPolicyNamespaceDto[];
 }
