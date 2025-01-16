f7b0572a0a9617aa83430649c76dc17914d54777
Behrang Alavi <behrang@mogenius.com>
2025-01-16 14:53:52 +0100

    fix: add K8sGetWorkspaceResponseDto

--- src/modules/mo-kubernetes/data-transfer-objects/k8s-workspace/index.ts
+++ src/modules/mo-kubernetes/data-transfer-objects/k8s-workspace/index.ts
@@ -1,6 +1,7 @@
 export * from './k8s-create-workspace.request.dto';
 export * from './k8s-delete-workspace.request.dto';
 export * from './k8s-get-workspace.request.dto';
+export * from './k8s-get-workspace.response.dto';
 export * from './k8s-get-workspaces.response.dto';
 export * from './k8s-update-workspace.request.dto';
 export * from './k8s-workspace-resource.dto';
--- src/modules/mo-kubernetes/data-transfer-objects/k8s-workspace/k8s-get-workspace.response.dto.ts
+++ src/modules/mo-kubernetes/data-transfer-objects/k8s-workspace/k8s-get-workspace.response.dto.ts
@@ -3,8 +3,8 @@ import { K8sMessageResponseBaseDto } from '../k8s-message-response-base.dto';
 import { MoUtils } from '@mogenius/js-utils';
 import { K8sWorkspaceDto } from './k8s-workspace.dto';
 
-export class K8sGetWorkspacesResponseDto extends K8sMessageResponseBaseDto<K8sWorkspaceDto[]> {
-  @Transform(({ value }) => MoUtils.transformToDtoList(K8sWorkspaceDto, value ?? []))
+export class K8sGetWorkspaceResponseDto extends K8sMessageResponseBaseDto<K8sWorkspaceDto> {
+  @Transform(({ value }) => MoUtils.transformToDto(K8sWorkspaceDto, value ?? []))
   @Expose()
-  data: K8sWorkspaceDto[];
+  data: K8sWorkspaceDto;
 }
