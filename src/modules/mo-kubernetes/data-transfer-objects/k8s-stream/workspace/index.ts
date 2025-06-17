1792861ea7dd272bf208cbadb4a869224e93a41b
Behrang Alavi <behrang@mogenius.com>
2025-06-17 10:31:28 +0200

    fix: pod stream dtos

--- src/modules/mo-kubernetes/data-transfer-objects/k8s-stream/workspace/index.ts
+++ src/modules/mo-kubernetes/data-transfer-objects/k8s-stream/workspace/index.ts
@@ -3,3 +3,6 @@ export * from './k8s-stream-workspace-container-shell.config.dto';
 export * from './k8s-stream-workspace-event-log.config.dto';
 export * from './k8s-stream-workspace-operator-log.config.dto';
 export * from './k8s-stream-workspace-helm-operator-log.config.dto';
+export * from './k8s-stream-workspace-pod-traffic.config.dto';
+export * from './k8s-stream-workspace-pod-cpu.config.dto';
+export * from './k8s-stream-workspace-pod-memory.config.dto';
---
+++ src/modules/mo-kubernetes/data-transfer-objects/k8s-stream/workspace/k8s-stream-workspace-pod-traffic.config.dto.ts
@@ -0,0 +1,32 @@
+import { K8sStreamBaseConfigDto } from '../k8s-stream-base-config.dto';
+import { Expose, Transform } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { K8sStreamRequestTypeEnum } from '../../../enums/k8s-stream/k8s-stream-request-type.enum';
+import { K8sStreamCmdEnum } from '../../../enums/k8s-stream/k8s-stream-cmd.enum';
+
+export class K8sStreamWorkspacePodTrafficConfigDto extends K8sStreamBaseConfigDto {
+  @Transform(() => K8sStreamRequestTypeEnum.WORKSPACE__LIVE_STREAM_POD_TRAFFIC)
+  @IsEnum(K8sStreamRequestTypeEnum)
+  @Expose()
+  type: K8sStreamRequestTypeEnum.WORKSPACE__LIVE_STREAM_POD_TRAFFIC;
+
+  @Transform(() => K8sStreamCmdEnum.STREAM)
+  @IsEnum(K8sStreamCmdEnum)
+  @Expose()
+  cmd: K8sStreamCmdEnum;
+
+  @IsNotEmpty()
+  @IsString()
+  @Expose()
+  workspaceName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Expose()
+  nodeName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Expose()
+  podName: string;
+}
